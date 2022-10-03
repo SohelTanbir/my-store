1d1a6998c096f260ddfc30adb0286c01a44a6ad7
SohelTanbir <soheltanbir19@gmail.com>
2022-10-03 15:58:21 +0600

    mongodb database connected with app

--- Back End/config/DatabaseConnection.js
+++ Back End/config/DatabaseConnection.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = ()=>{
-  
+    mongoose.connect(process.env.DB_URI, {}, (data)=>{
+        console.log("Database Connected");
+    })
 }
 
 
