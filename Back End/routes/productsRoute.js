5d5bf315ba705501303dd29f812e3e83c5cd9c8e
SohelTanbir <soheltanbir19@gmail.com>
2022-10-22 00:12:56 +0600

    role based authorization api done

--- Back End/routes/productsRoute.js
+++ Back End/routes/productsRoute.js
@@ -8,14 +8,15 @@ const {
     deleteProduct, 
     getProductById,
     updateProduct } = require("../controllers/productController");
+const authorization = require("../middleware/auth");
 const checkLogin = require("../middleware/checkLogin");
 
 // Product routes
 router.get("/product/all", getAllProducts);
 router.get("/product/one/:id", getProductById);
-router.post("/product/create", checkLogin, addNewProduct);
-router.delete("/product/delete/:id",checkLogin, deleteProduct);
-router.put("/product/update/:id",checkLogin, updateProduct);
+router.post("/product/create", checkLogin,authorization, addNewProduct);
+router.delete("/product/delete/:id",checkLogin,authorization, deleteProduct);
+router.put("/product/update/:id",checkLogin,authorization, updateProduct);
 
 
 // export routes
