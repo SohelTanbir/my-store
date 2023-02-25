36514d9e6a93db756509f81309c6f55e244ddf0d
SohelTanbir <soheltanbir19@gmail.com>
2023-02-25 12:14:20 +0600

    design sidebar right menu

--- Front End/src/components/RightNav/RightNav.js
+++ Front End/src/components/RightNav/RightNav.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import './RightNav.css';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Link } from 'react-router-dom';
+import userPhoto from '../../images/users/sohelrana.jpg';
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft, faCompass, faEnvelope, faPowerOff, faUser } from '@fortawesome/free-solid-svg-icons';
+
 
 const RightNav =  ({showNav, setShowNav}) =>{
 
@@ -9,11 +14,58 @@ const RightNav =  ({showNav, setShowNav}) =>{
   return (
     <>
       <Offcanvas show={showNav} onHide={handleClose} placement="end" id="Right-menu">
-        <Offcanvas.Header closeButton>
+        <Offcanvas.Header>
+          <div className="login-user">
+                <img src={userPhoto} alt="user" />
+                <h4>Sohel Rana</h4>
+          </div>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
+
+        <ul className='sidebar-right-menu'>
+                <li>
+                  <Link to="/users/profile">
+                    <div className="left-icon">
+                      <FontAwesomeIcon icon={faUser}/>
+                    </div>
+                      Profile
+                    </Link>
+                </li>
+                <li>
+                  <Link to="/orders/track">
+                    <div className="left-icon">
+                      <FontAwesomeIcon icon={faCompass}/>
+                    </div>
+                    Track order
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/contact">
+                  <div className="left-icon">
+                      <FontAwesomeIcon icon={faEnvelope}/>
+                    </div>
+                    Contact
+                  </Link>
+                </li>                                           
+                <li>
+                  <Link to="/return/policy">
+                    <div className="left-icon">
+                      <FontAwesomeIcon icon={faEnvelope}/>
+                    </div>
+                    Easy Return
+                  </Link>
+                </li>
+              
+                <li>
+                  <Link to="#">
+                  <div className="left-icon">
+                      <FontAwesomeIcon icon={faPowerOff}/>
+                    </div>
+                    Log out
+                  </Link>
+                </li>
+      </ul>
+      
         </Offcanvas.Body>
       </Offcanvas>
     </>
