c2a16d52e1cb3e9711a73c851f16ecb270ae988b
SohelTanbir <soheltanbir19@gmail.com>
2023-05-13 17:08:49 +0600

    validate forgot password email field

--- Front End/src/components/ResetPassword/ForgotPassword.js
+++ Front End/src/components/ResetPassword/ForgotPassword.js
@@ -4,19 +4,37 @@ import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Loader from '../Loader/Loader';
 import Header from '../Header/Header'
+import { isValidEmail } from '../../utilities';
 
 
 const ForgotPassword = () => {
     const [email, setEmaqil ] = useState("");
     const [loader, setLoader] =  useState(false);
-
+    const [showError, setShowError] = useState({});
     const handleChange = (e)=>{
         setEmaqil(e.target.value);
+    
+            if (!isValidEmail(email)) {
+              setShowError({ email: "Invalid email address!" });
+              return "";
+            }
+            setShowError({ email: "" });
     }
     // submit email and get the reset password url
     const handleSubmit = async e =>{
-        setLoader(true);
         e.preventDefault();
+    // email validation
+    if (!email) {
+        setShowError({ email: "Please enter email address!" });
+        return;
+      }
+      setShowError({ email: "" });
+      if (!isValidEmail(email)) {
+        setShowError({ email: "Invalid email address!" });
+        return "";
+      }
+      setShowError({ email: "" });
+      setLoader(true);
        if(email.length > 0){
         const response = await  fetch("http://localhost:5000/api/v1/users/password/forgot",{
             method:'post',
@@ -49,7 +67,12 @@ const ForgotPassword = () => {
                         <p>Please Enter your email, we will send a link to reset your password</p>
                         <form onSubmit={handleSubmit}>
                             <input type="text" onChange={handleChange} placeholder='Enter your email '    /> <br />
-                            <button className='reset-password-btn'>Reset link</button>
+                            {showError.email && (
+                            <p className="text-danger fw-bold p-2 fs-5 text-start">
+                            {showError.email}
+                            </p>
+                        )}
+                            <button className='reset-password-btn mt-4'>Reset link</button>
                         </form>
                     </div>
                 </div>
